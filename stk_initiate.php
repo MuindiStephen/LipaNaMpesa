c00786dc1d24163184684e9efc7c24e44ee83c4f
stephenmwania2000@gmail.com <stephenmuindi241@gmail.com>
2022-04-10 15:13:33 +0300

    changes

--- stk_initiate.php
+++ stk_initiate.php
@@ -24,7 +24,7 @@ if(isset($_POST['submit'])){
   */
   
    $PartyA = $_POST['phone']; // This is your phone number, 
-  $AccountReference = '2255';
+  $AccountReference = $_POST['2255'];
   $TransactionDesc = 'Test Payment';
   $Amount = $_POST['amount'];;
  
